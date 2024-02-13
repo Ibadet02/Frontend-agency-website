bc042ebf2a977870d5f35291ba929586d680cdbf
Ibadet <ibadetismayilov20@gmail.com>
2024-02-13 19:06:57 +0400

    added scrollbar

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -14,12 +14,36 @@ export const GlobalStyle = createGlobalStyle<{ theme: Theme }>`
     }
     html{
         /* font-family: 'Poppins', sans-serif; */
+        ::-webkit-scrollbar {
+            width: .6rem;
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+            border-radius: 1rem;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: ${({ theme }) => theme.colors().mainBlue};
+            border-radius: 1rem;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+            background: #2552cf;
+        }
+        ::-webkit-scrollbar-thumb:active {
+            background: #1a47c6;
+        }
         scroll-behavior: smooth;
     }
     body {
         padding-top: calc(${({ theme }) => theme.heights.navbar} + 1rem);
         background-color: ${({ theme }) => theme.colors().mainBG()};
-        transition: background-color ${({ theme }) => theme.transitions.changeMode};
-        overflow: ${({ theme})=> theme.bodyOverflow};
+        transition: background-color ${({ theme }) =>
+          theme.transitions.changeMode};
+        overflow: ${({ theme }) => theme.bodyOverflow};
     }
 `;
