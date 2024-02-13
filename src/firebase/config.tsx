5b7faf12ab319683e7e5c5c11ff6f3133d73def3
Ibadet <ibadetismayilov20@gmail.com>
2024-02-13 18:39:01 +0400

    npm run build

--- src/firebase/config.tsx
+++ src/firebase/config.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 // import firebase from 'firebase/app';
-import {collection, getFirestore} from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,7 +12,7 @@ const firebaseConfig = {
   projectId: "devro-607ff",
   storageBucket: "devro-607ff.appspot.com",
   messagingSenderId: "316879452723",
-  appId: "1:316879452723:web:1a6c1906968d6970964146"
+  appId: "1:316879452723:web:1a6c1906968d6970964146",
 };
 
 // Initialize Firebase
