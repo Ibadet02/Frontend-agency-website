133078f75fdce211b5e02683e74003550fff98e4
Ibadet <ibadetismayilov20@gmail.com>
2024-02-21 23:56:14 +0100

    added Rober's text to About us

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -26,7 +26,13 @@ const About = () => {
             <Reveal withSlide>
               <h2 className="name">Robert Zet</h2>
             </Reveal>
-            <MainDescription text="lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum." />
+            <MainDescription
+              text="Greetings! Welcome to our digital home! I’m Robert, the brain behind this agency.
+              If you want to picture me, I will help you: A passionate individual to creates meaningful relationships with people and fosters a great community with people who share the same passion and excitement as me. Yep, that’s me in a nutshell. 
+              So, as you navigate through our virtual home, I invite you to join us on this crazy adventure of discovery, connection and passion. 
+              Oh, I almost forgot! I am not alone in this crazy journey, check the guy to my right! He’s a handsome fella! 
+              Cheers, mate!"
+            />
           </div>
           <div className="info-frame">
             {/* <Image {...secondImageProps} /> */}
